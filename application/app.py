5f5aaf047ec1447860f196280bbd7c6bc09b823d
jellevink <jelle.vinkenoog@academytrainee.com>
2020-01-08 16:53:01 +0000

    needed app.py in application folder

--- application/app.py
+++ application/app.py
@@ -1,6 +1,5 @@
-import numpy as np
+from application import app
 
-rand_int=np.random.randint(1,100,1)
-
-print(rand_int)
-
+#runs in the debug mode and allows for connections from all IPs
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
