5bf3f745069b0a592120a616bb27fc25ccb8d893
jellevink <jelle.vinkenoog@academytrainee.com>
2020-01-09 23:48:00 +0000

    addded connection to __init__.py and environment variables to allow for connection to the mysql database

--- flask/application/__init__.py
+++ flask/application/__init__.py
@@ -1,5 +1,25 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+from flask_bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
+
+#Set environment variables to connect to the correct mySQL database hosted on a GCP instance
+sql_user = getenv('MYSQL_USER')
+sql_pass = getenv('MYSQL_PASSWORD')
+sql_url = getenv('MYSQL_HOST')
+sql_db = getenv('MYSQL_DB')
+sql_key = getenv('YOUR_SECRET_KEY')
+#Connect to the database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + sql_user + ':' + sql_pass + '@' + sql_url + '/' + sql_db
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['SECRET_KEY'] = sql_key
+#Initiates the database
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 from application import routes
