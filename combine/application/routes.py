9ef1c940f0e84252be8e0ffa051c6ad5842bc3f3
jellevink <jelle.vinkenoog@academytrainee.com>
2020-01-10 12:39:15 +0000

    fixes errors between docker-compose.yaml and Dockerfiles and attempting to solve correct services to pull from with Docker

--- combine/application/routes.py
+++ combine/application/routes.py
@@ -5,17 +5,11 @@ import os
 import random
 import string
 
-"""@app.route("/makerandomsequence", methods=["POST"])
-def makesequence():
-    sequence = ""
-    sequence = requests.post( "http://flask-number1:5000/randomnumber1")
-    randnumber2 = requests.post( "http://flask-number2:5000/randomnumber2")
-    return {"sequence":sequence}"""
 
 @app.route('/', methods=["GET","POST"])
 def combine():
-    random_number1 = int(requests.get('http://localhost:5001').text)
-    random_number2 = int(requests.get('http://localhost:5002').text)
+    random_number1 = int(requests.get('http://random1:5001').text)
+    random_number2 = int(requests.get('http://random2:5002').text)
     print(random_number1)
     print(random_number2)
     #randoms = str(int(random_number1)+int(random_number2))
