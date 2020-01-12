92b49461b01712c382a7480ea8e7c1ae8610c6e7
jellevink <jelle.vinkenoog@academytrainee.com>
2020-01-12 21:52:31 +0000

    attempting pytest coverage

--- random1/tests/tests.py
+++ random1/tests/tests.py
@@ -1,6 +1,6 @@
 import pytest
 import unittest
-from application import routes 
+from application import app, routes 
 
 def test_random_number1():
     assert routes.makerandomnum1()=="1" or "2" or "3" or "4" or "5" or "6" or "7" or "8" or "9" or "10"
